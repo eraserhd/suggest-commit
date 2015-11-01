8846322219fec14475e5e7b4858311def3067ffc
Jason Felice <jason.m.felice@gmail.com>
2015-11-01 14:04:27 -0500

    Remove duplicate test

--- t_tests.hh
+++ t_tests.hh
@@ -7,9 +7,4 @@ public:
     {
         TS_ASSERT_EQUALS("midje is awesome!", test_name("\t (fact\"midje is awesome!\" "));
     }
-
-    void test_Detects_cxxtest_facts() {
-        TS_ASSERT_EQUALS("Detects cxxtest facts", test_name("void te" "st_Detects_cxxtest_facts()"));
-    }
-
 };
