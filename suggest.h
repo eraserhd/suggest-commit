11730dfd27383c1bdc4f67770aec0e24415edfd0
Jason Felice <jason.m.felice@gmail.com>
2015-10-31 22:54:19 -0400

    Finds some cxxtest facts

--- suggest.h
+++ suggest.h
@@ -1,6 +1,7 @@
 #ifndef suggest_h_INCLUDED
 #define suggest_h_INCLUDED
 
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <regex>
@@ -26,13 +27,23 @@ int edit_distance(std::string const& a, std::string const& b)
     return dp[a.size()][b.size()];
 }
 
+std::string decode_JUnit_style_name(std::string const& name)
+{
+    std::string result(name);
+    std::replace(result.begin(), result.end(), '_', ' ');
+    return result;
+}
+
 std::string test_name(std::string const& line)
 {
     static const std::regex midje_fact("\\s*\\(fact\\s*\"(.*)\"\\s*");
+    static const std::regex cxxtest_fact("\\s*void\\s+test_?([A-Za-z0-9_]+)\\s*\\(\\s*\\)\\s*");
 
     std::smatch matches;
     if (std::regex_match(line, matches, midje_fact))
         return matches.str(1);
+    if (std::regex_match(line, matches, cxxtest_fact))
+        return decode_JUnit_style_name(matches.str(1));
 
     return "";
 }
--- t_tests.h
+++ t_tests.h
@@ -1,11 +1,15 @@
 #include <cxxtest/TestSuite.h>
 #include "suggest.h"
 
-class t_tests : public CxxTest::TestSuite
-{
+class t_tests : public CxxTest::TestSuite {
 public:
     void test_Detects_midje_facts()
     {
         TS_ASSERT_EQUALS("midje is awesome!", test_name("\t (fact\"midje is awesome!\" "));
     }
+
+    void test_Detects_cxxtest_facts() {
+        TS_ASSERT_EQUALS("Detects cxxtest facts", test_name("void te" "st_Detects_cxxtest_facts()"));
+    }
+
 };
